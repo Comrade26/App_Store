924a6bc404c2e35465775c2731db681d7bc32635
Pooote <noppakrit2545@gmail.com>
2023-04-11 16:28:30 +0700

    model buyer

---
+++ Buyer/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('buyer/signup/', views.buyer_signup, name='buyer_signup'),
+    # path('buyer/login/', views.buyer_login, name='buyer_login'),
+    # path('buyer/profile/', views.buyer_profile, name='buyer_profile'),
+]
