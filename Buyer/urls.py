844f967429a802f1c0769a8a21a9979666114e55
Goman <gomann1178@gmail.com>
2023-04-14 21:35:25 +0700

    Revert "Merge remote-tracking branch 'origin/Poote_Branch'"
    This reverts commit d02df83e4eedf3fdfc66ed1969cb92b7166b5adc, reversing
    changes made to 5eaa193213441db29e67600b188587d86469a106.

--- Buyer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('buyer/signup/', views.buyer_signup, name='buyer_signup'),
-    # path('buyer/login/', views.buyer_login, name='buyer_login'),
-    # path('buyer/profile/', views.buyer_profile, name='buyer_profile'),
-]
