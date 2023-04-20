233f532113e398cfbb529d5bbb50f61e319515ed
Comrade26 <120408138+Comrade26@users.noreply.github.com>
2023-04-20 18:09:47 +0700

    basic login

--- Buyer/urls.py
+++ Buyer/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from . import views
+from .views import BuyerLoginView
 
 urlpatterns = [
-    # path('buyer/signup/', views.buyer_signup, name='buyer_signup'),
-    # path('buyer/login/', views.buyer_login, name='buyer_login'),
-    # path('buyer/profile/', views.buyer_profile, name='buyer_profile'),
+    path('login/', BuyerLoginView.as_view(), name='buyer_login'),
 ]
--- Buyer/views.py
+++ Buyer/views.py
@@ -1,46 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView
-from django.contrib.auth import authenticate, login, logout
-from .models import Customer
-from django.shortcuts import render, redirect
-# from .forms import BuyerLoginForm
 
-# Create your views here.
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             customer = Customer.objects.create(
-#                 user=user,
-#                 phone_number=request.POST.get('phone_number'),
-#                 address=request.POST.get('address'),
-#                 city=request.POST.get('city'),
-#                 state=request.POST.get('state'),
-#                 country=request.POST.get('country'),
-#             )
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('home')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'signup.html', {'form': form})
-
-# def buyer_login(request):
-#     if request.method == 'POST':
-#         form = BuyerLoginForm(data=request.POST)
-#         if form.is_valid():
-#             user = form.get_user()
-#             login(request, user)
-#             return redirect('buyer_profile')
-#     else:
-#         form = BuyerLoginForm()
-#     return render(request, 'buyer_login.html', {'form': form})
-
-# def buyer_logout(request):
-#     logout(request)
-#     return redirect('home')
+class BuyerLoginView(LoginView):
+    template_name = 'login.html'
