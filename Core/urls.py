dae848019f4fbc03e2355bf785040d89f8528150
Comrade26 <120408138+Comrade26@users.noreply.github.com>
2023-04-24 13:22:28 +0700

    add register button to login page

--- Core/urls.py
+++ Core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import RegistrationView
 
+app_name = 'Core'
+
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
 ]
