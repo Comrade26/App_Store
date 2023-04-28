ef65475f5db13c51b9f359747e76736d82322ac4
Comrade26 <120408138+Comrade26@users.noreply.github.com>
2023-04-28 19:14:40 +0700

    edit profile

--- Core/urls.py
+++ Core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', BuyerLoginView.as_view(), name='login'),
     path('register/', RegistrationView.as_view(), name='register'),
     path('profile/', views.profile, name='profile'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
+    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
 ]
--- Core/views.py
+++ Core/views.py
@@ -1,10 +1,12 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.contrib.auth.forms import PasswordChangeForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import RegistrationForm
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegistrationForm, EditProfileForm
+from .models import User, chula_email_validator
+
 
 class BuyerLoginView(LoginView):
     template_name = 'login.html'
@@ -18,3 +20,23 @@ def profile(request):
     user = request.user
     context = {'user': user}
     return render(request, 'profile.html', context)
+
+def edit_profile(request):
+    user = request.user
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('Core:profile')
+    else:
+        form = EditProfileForm(instance=user)
+    return render(request, 'edit_profile.html', {'form': form})
+
+class ChangePasswordView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('Core:profile')
+    template_name = 'change_password.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Your password has been changed.')
+        return super().form_valid(form)
