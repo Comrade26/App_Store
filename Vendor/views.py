2b4adbc68ee1bd60bce80b28c46e32d4cf667659
Comrade26 <120408138+Comrade26@users.noreply.github.com>
2023-04-28 20:10:33 +0700

    ???

--- Vendor/views.py
+++ Vendor/views.py
@@ -5,7 +5,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from .forms import VendorRegistrationForm
+# from .forms import VendorRegistrationForm
 from .models import Category, Product, Vendor
 
 def vendor_login(request):
