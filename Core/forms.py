7d758a072ac4a58b71448a209ad0ba38a2eb87f8
Comrade26 <120408138+Comrade26@users.noreply.github.com>
2023-05-09 19:15:35 +0700

    add profile vendor and edit vendor

--- Core/forms.py
+++ Core/forms.py
@@ -4,6 +4,7 @@ from django.db import models
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from .models import User, chula_email_validator
+from Vendor.models import Vendor
 
 
 class RegistrationForm(UserCreationForm):
@@ -38,11 +39,35 @@ class RegistrationForm(UserCreationForm):
         return user
 
 class EditProfileForm(forms.ModelForm):
-    class Meta:
+    class Meta: 
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'telephone', 'address']
 
+class EditVendorForm(forms.ModelForm):
+    store_name = forms.CharField(max_length=100, required=True)
+    location = forms.CharField(max_length=100, required=True)
+    store_picture = forms.ImageField(required=False)
+    qr_picture = forms.ImageField(required=False)
 
+    class Meta:
+        model = Vendor
+        fields = ['store_name', 'location', 'store_picture', 'qr_picture']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)
+        if instance:
+            self.fields['store_name'].initial = instance.store_name
+            self.fields['location'].initial = instance.location
+            self.fields['store_picture'].initial = instance.store_picture
+            self.fields['qr_picture'].initial = instance.qr_picture
 
-
+    def save(self, commit=True):
+        vendor = super().save(commit=False)
+        vendor.store_name = self.cleaned_data['store_name']
+        vendor.location = self.cleaned_data['location']
+        vendor.store_picture = self.cleaned_data['store_picture']
+        vendor.qr_picture = self.cleaned_data['qr_picture']
+        if commit:
+            vendor.save()
+        return vendor
